f5c7ec3a34dee69c86506ac5ea39fbb3f3dcbde1
AnastasiusW <repositoryanastasiia@gmail.com>
2021-04-05 15:19:18 +0300

    Task two

--- task_2/array.rb
+++ task_2/array.rb
@@ -1,2 +1,21 @@
+require 'pry'
 class Array
+
+  def rebuild!(&block)
+    result = []
+
+    self.each do |element|
+      if element.kind_of?(Array)
+        res = []
+        size_array = element.size
+      element.each do |el|
+        res << block.call(el,size_array)
+      end
+      result << res
+      else
+        result << block.call(element,self.size)
+      end
+    end
+    result
+  end
 end
